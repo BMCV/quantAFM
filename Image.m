b6b2d56e34754d02df4da6021f839889e41f5019
Christian Schuetz <christian.schuetz@stud.uni-heidelberg.de>
2016-05-25 20:48:19 +0200

    Restructuring & commenting; add opencv denoising
    The opencv denoising function increases the amount
    of detected DNA fragments lateron. However, runtime
    increases, and sometimes single fragments are not
    detected or only as two fragments. So tweaking still
    needs to be done. For use, see comments in code.

--- Image.m
+++ Image.m
@@ -10,22 +10,38 @@ classdef Image
         
         connected;
         region;
-        
-        image;
+
+        % original image as read
+        rawImage;
         colorMap;
-        bwImage;
-        bwImage1;
-        bwImageremoved;
-        medImage;
+        % bw image used for artifact removal
+        bwImage;      
+        % first final bw image containing mostly thick DNA fragments / nucleosomes
+        bwImgThickDna;
+        % denoised and filtered bw image
+        bwImgDen;
+        % denoised, filtered and thinned bw image
+        bwImgDenThinned;
+        % bw image where artifacts have been filtered out
+        bwFilteredImage;
+        % original image after first preprocessing steps (lowpass + median filtered)
+        preprocImg;
+        % original image after denoising with OpenCV function
+        denoisedImg;
+        
         fftImage;
         fftbwImage;
         cArray;
         metaImage;
-        complementImage;
-        cleanImage;
+        % original image where artifacts' intensity is adjusted
+        filteredImage; 
+        % background of original image
         background;
-        thinImage;
-        centers,
+        % first final bw image containing 1 px thick DNA fragments, 
+        % no nucleosomes visible anymore 
+        bwImgThinnedDna; 
+        
+        centers;
         radii;
         
         
@@ -37,7 +53,7 @@ classdef Image
             imageObj.mutantDnaCount = 0;
             imageObj.normalDnaCount = 0;
             if nargin >0
-                imageObj.image = image;
+                imageObj.rawImage = image;
                 imageObj.colorMap = colorMap;
             end
         end
