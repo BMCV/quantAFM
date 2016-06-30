152598acf0db5f423170d1ca79e2f97c3b26b2f4
Metadelee <lina.gundelwein@yahoo.de>
2016-06-30 14:29:12 +0200

    measure angle and display methods

--- measure_angle.m
+++ measure_angle.m
@@ -1,90 +1,82 @@
 function [angle1,angle2] = measure_angle(dna)
 %% 
 % dna: dna_bound, dna.attachedNukleo, dna.attachedNukleo.rad, center
-% imsize: size of the original image. used to convert linear indices
+
 angle1 = 0;
 angle2 = 0;
 
 
-if numel(dna.attachedNukleo) > 1
-    % more than one center!
+if numel(dna.attachedNukleo) ~= 1
+    % more than one center or no center!
     return
-elseif numel(dna.attachedNukleo) == 0
-        return
 end
 
-% coordinates of DNA
-[x,y] = ind2sub(dna.sizeImg,dna.connectedThinned);
-coords = [x,y];
+% cut out nucleo
+dnaImgThinned = dna.bwImageThinned;
+mask = bsxfun(@plus, ((1:dna.sizeImg(2)) - dna.attachedNukleo{1}.localCenter(1)).^2, ((transpose(1:dna.sizeImg(1)) - dna.attachedNukleo{1}.localCenter(2)).^2)) < dna.attachedNukleo{1}.rad^2;
+dnaImgThinned(mask) = 0;
 
+% Get the two branches
+arms = bwconncomp(dnaImgThinned);
 
-% Distance of each DNA pixel to center of nukleus
-D = pdist2(coords,dna.attachedNukleo{1}.localCenter);
-D1 = D;
-
+% If there are more or less than 2 arms, return
+if  numel(arms.PixelIdxList) ~= 2
+    return
+end
+    
 %% case 1: angle between intersecting pixel and centerpoint
+% get intersecting points
+[x1,y1] = ind2sub(dna.sizeImg,arms.PixelIdxList{1});
+[x2,y2] = ind2sub(dna.sizeImg,arms.PixelIdxList{2});
+[~, idx1] = pdist2([y1,x1], dna.attachedNukleo{1}.localCenter,'euclidean', 'Smallest',1);
+[~, idx2] = pdist2([y2,x2], dna.attachedNukleo{1}.localCenter,'euclidean','Smallest',1);
+p = [y1(idx1),x1(idx1); y2(idx2), x2(idx2)];
+% get lines throug points & nucleo center
+a = dna.attachedNukleo{1}.localCenter-p(1,:);
+b = dna.attachedNukleo{1}.localCenter- p(2,:);
+% get angle
+angle1 = acosd(a*b'/(norm(a)*norm(b)));
+% show angle
+% showAngle
 
-    coords(D1<dna.attachedNukleo{1}.rad,:) = [];
-    D1(D1<dna.attachedNukleo{1}.rad) = [];
-    if size(coords,1) >1
-        [~,idx_min] = sort(D1);
-        % interesecting points
-        p = coords(idx_min(1:2),:);
-
-        angle1 = atan2d(abs(det([p(1,:)-dna.attachedNukleo{1}.localCenter;p(2,:)-dna.attachedNukleo{1}.localCenter])),...
-            dot(p(1,:)-dna.attachedNukleo{1}.localCenter,p(2,:)-dna.attachedNukleo{1}.localCenter));
-    end
 
 %% case 2: angle between lines fitted on arms
-    x1 = x;
-    y1 = y;
-    % Delete everything inside of nukleus and outside of "outside circle"
-    x1(D<dna.attachedNukleo{1}.rad|D>dna.attachedNukleo{1}.rad+5) = [];
-    y1(D<dna.attachedNukleo{1}.rad|D>dna.attachedNukleo{1}.rad+5) = [];
+% Delete everything outside of "outside circle"
+mask = bsxfun(@plus, ((1:dna.sizeImg(2)) - dna.attachedNukleo{1}.localCenter(1)).^2, ((transpose(1:dna.sizeImg(1)) - dna.attachedNukleo{1}.localCenter(2)).^2)) < (dna.attachedNukleo{1}.rad+5)^2;
+dnaImgThinned = dnaImgThinned.*mask;
 
-    % Create image with two (or unfortunately more) DNA strings to fit line to
-    arms = false(dna.sizeImg);
-    arms(sub2ind(dna.sizeImg, x1, y1)) = true;
+% Get the two branch stumps
+arms = bwconncomp(dnaImgThinned);
 
-    % workaround more than one arm
-    arms_ = bwconncomp(arms);
 
-    if arms_.NumObjects > 2
-%         numPixels = cellfun(@numel,arms_.PixelIdxList);
-%         [~,idx] = sort(numPixels,'descend');
-%         arms( arms_.PixelIdxList{idx(3:end)}) = 0;
-%         arms_ = bwconncomp(arms);
+if  size(arms.PixelIdxList{1},1)>1 && size(arms.PixelIdxList{2},1)>1
+    % get coordinates of arms
+    [x1,y1] = ind2sub(dna.sizeImg,arms.PixelIdxList{1});
+    [x2,y2] = ind2sub(dna.sizeImg,arms.PixelIdxList{2});
+    % Points furthest away from nucleo center
+    [~, idx1] = pdist2([y1,x1], dna.attachedNukleo{1}.localCenter,'euclidean', 'Largest',1);
+    [~, idx2] = pdist2([y2,x2], dna.attachedNukleo{1}.localCenter,'euclidean','Largest',1);
+
+    % fit lines to branch stumps
+    p1 = polyfit(x1,y1,1);
+    p2 = polyfit(x2,y2,1);
+    if p1(1) == p2(1)
+        angle2 = 180;
+    else
+        p = [polyval(p1,x1(idx1)),x1(idx1); polyval(p2,x2(idx2)),x2(idx2)];
+        % get intersection of the two lines
+        x_intersect = fzero(@(x) polyval(p1-p2,x),3);
+        y_intersect = polyval(p1,x_intersect);
+        centers = [y_intersect, x_intersect];
+        a = centers-p(1,:);
+        b = centers- p(2,:);
+        % get angle
+        angle2 = acosd(a*b'/(norm(a)*norm(b)));
+        % show Angle
+        % showAngle2
     end
-    warning off
-    if numel(arms_.PixelIdxList) ==2 && size(arms_.PixelIdxList{1},1)>1 && size(arms_.PixelIdxList{2},1)>1
-        % get coordinates of arms
-        [x1,y1] = ind2sub(dna.sizeImg,arms_.PixelIdxList{1});
-        [x2,y2] = ind2sub(dna.sizeImg,arms_.PixelIdxList{2});
-        p1 = polyfit(x1,y1,1);
-        p2 = polyfit(x2,y2,1);
-        if p1(1) == p2(1)
-            angle2 = 180;
-        else
-            p = [x1(1),polyval(p1,x1(1));x2(end), polyval(p2,x2(end))];
-            x_intersect = fzero(@(x) polyval(p1-p2,x),3);
-            y_intersect = polyval(p1,x_intersect);
-            centers = [x_intersect, y_intersect];
-            angle2 = atan2d(abs(det([p(1,:)-centers;p(2,:)-centers])),dot(p(1,:)-centers,p(2,:)-centers));            
-%             close all
-%             imshow(arms);
-%             hold on;
-%             scatter(p(:,2),p(:,1), 'o','red')
-%             plot(polyval(p1,x1),x1);
-%             plot(polyval(p2,x2),x2);
-%             scatter(centers(2),centers(1),'x','cyan')
-%             scatter(dna.attachedNukleo{1}.localCenter(2),dna.attachedNukleo{1}.localCenter(1),'o','blue')
-%              disp('---')
-%              disp(angle1)
-%              disp(angle2)
-%             pause
-        end
-    end
-    warning on
+end
+
 
 end
 
