396129aa21e6b8c07f4e9588a3565ae5a3196bc8
Christian Schuetz <christian.schuetz@stud.uni-heidelberg.de>
2016-07-21 17:45:14 +0200

    Remove .cvs from Repo,please don't add them in code base

--- correlateResults.m
+++ correlateResults.m
@@ -1,8 +1,9 @@
 function done = correlateResults(idx, a, b, imgObj)
     
-    path1 = ['../dokumentation/Ergebnisse/fastThinning_ChristianLength/denoised_' idx '_fast_ChrLen.csv'];
+    path1 = ['../dokumentation/Ergebnisse/auswertungen/fastThinning_ChristianLength/denoised_' idx '.tif_fast_Chr.csv'];
     path2 = ['../handauswertungen/' idx '.xlsx'];
-    target = ['correlation_' idx '.csv'];
+    target1 = ['correlation1_' idx '.csv'];
+    target2 = ['correlation2_' idx '.csv'];
     len = imgObj.imgSize(1);
     t1 = csvread(path1,1,1);
     t2 = xlsread(path2);
@@ -14,12 +15,17 @@ function done = correlateResults(idx, a, b, imgObj)
     vec1 = [x1, y1];
     vec2 = [x2, y2];
     dists = pdist2(vec1, vec2);
-    [M,I] = min(dists);
-    numbers = 1:size(I,2);
-    correlation = [I;numbers];
-    csvwrite(target, correlation);
+    dists2 = pdist2(vec2, vec1);
+    [M1,I1] = min(dists);
+    [M2,I2] = min(dists2);
+    numbers1 = 1:size(I1,2);
+    numbers2 = 1:size(I2,2);
+    correlation1 = [I1;numbers1];
+    correlation2 = [I2;numbers2];
+    csvwrite(target1, correlation1);
+    csvwrite(target2, correlation2);
     
-    showResults(t2, a,b, len, imgObj);
+    %showResults(t2, a,b, len, imgObj);
 end
 
 function showResults(t2, a,b, imgLen, imgObj)
